738dbcb6ba059ba6b4b90fc5c3b2682058a4ed44
Alexander Petrov <ptrv.dev@gmail.com>
2023-05-13 15:57:07 +0300

    Added comments

--- src/pages/ProjectsPage/tabs/FilteredProjectsTab.js
+++ src/pages/ProjectsPage/tabs/FilteredProjectsTab.js
@@ -8,6 +8,9 @@ import ErrorBadge from "../../../components/ErrorBadge/ErrorBadge";
 const FilteredProjectsTab = () => {
 
     const {slug} = useParams();
+
+    // Filters do not work without "WP REST API Filter Parameter" plugin installed in the backend
+    // https://github.com/WP-API/rest-filter
     const {data, loading, error} = useFetch(`https://wordpress.ptrv.dev/wp-json/wp/v2/project?filter[technology]=${slug}&_embed&acf-format=standard&per_page=50`);
 
     return (  
